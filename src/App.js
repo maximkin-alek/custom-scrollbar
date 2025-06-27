f625455ef140e9f72adf440588ca904dc58bb71e
Alexander Maximkin <maximkin.alek@yandex.ru>
2025-06-27 11:15:18 +0300

    feat: hide inactive scroll

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { TEXT } from './data.ts';
 function App() {
   return (
     <div className="app">
-      <h1>Custom scrollbar</h1>
+      <h1 className="title">Custom scrollbar</h1>
 
-      <h2>Overlayscrollbars-react</h2>
+      <h2 className="title">Overlayscrollbars-react</h2>
       {/* Реализация библиотеки overlayscrollbars-react */}
       <OverlayScrollbarsComponent
         defer
@@ -23,13 +23,51 @@ function App() {
       </OverlayScrollbarsComponent>
 
       {/* Кастомный вариант */}
-      <h2>Custom</h2>
+      <h2 className="title">Custom</h2>
       <AppleScrollbar className="custom-container" scrollbarWidth={6} fadeTimeout={1000}>
         <div className="long-content">{TEXT}</div>
         <img
           src="https://i.pinimg.com/736x/e7/20/da/e720dae7575ae813e602940d5be8016c.jpg"
           alt=""
         />
+      </AppleScrollbar>
+
+      <h3 className="title">Горизонтальный скролл</h3>
+      <AppleScrollbar className="custom-container" scrollbarWidth={6} fadeTimeout={1000}>
+        <div style={{ display: 'flex', minWidth: 'max-content' }} className="">
+          {new Array(15).fill('_').map((_, i) => (
+            <div
+              style={{
+                minWidth: 100,
+                margin: '0 10px',
+                padding: '6px',
+                background: 'yellow',
+                borderRadius: 8,
+              }}
+            >
+              Блок №{i + 1}
+            </div>
+          ))}
+        </div>
+      </AppleScrollbar>
+
+      <h3 className="title">Вертикальный скролл</h3>
+      <AppleScrollbar className="custom-container" scrollbarWidth={6} fadeTimeout={1000}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }} className="">
+          {new Array(15).fill('_').map((_, i) => (
+            <div
+              style={{
+                minWidth: 100,
+                margin: '0 10px',
+                padding: '6px',
+                background: 'yellow',
+                borderRadius: 8,
+              }}
+            >
+              Блок №{i + 1}
+            </div>
+          ))}
+        </div>
       </AppleScrollbar>
     </div>
   );
